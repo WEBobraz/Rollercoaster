68ddf05668faa25a8794b46ec6693d33a0be82c5
WEBobraz <56693534+WEBobraz@users.noreply.github.com>
2023-09-07 00:15:52 +0000

    el/if+bill

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 print('Welcome to the rollercoaster!')
 height = int(input('What is your height in cm? '))
-
+#bill = 0
 if height >= 120:
-  print('You can ride the rollercoaster!')
-  age = int(input('How old are you? '))
-  if age < 12:
-    print('Your ticket coast $5')
-  elif age <= 18:
-    print('Your ticket coast $12')
-  else:
-    print('Your ticket coast $18')
+    print('You can ride the rollercoaster!')
+    age = int(input('How old are you? '))
+    if age < 12:
+            bill = 5
+            print('Child tickets are $5')
+    elif age <= 18:
+            bill = 7
+            print('Youth tickets are $7')
+    else:
+        bill = 12
+        print('Adult tickets are $12')
+    whants_picture = input('Do you whant a picture taken? Y or N. ')
+    if  whants_picture =='Y':
+        #Add $3 to their bill
+        #bill = bill + 3
+        bill += 3
+
+    print(f'Your total bill is ${bill}')
+
 else:
-  print('Sorry, you have to grow taller before you can ride.')
+    print('Sorry, you have to grow taller before you can ride.')
